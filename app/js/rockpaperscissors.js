b8c928f25d76a015464a7f894c46e41b09d2c7a5
Isabella <isabellaong@gmail.com>
2016-04-16 00:23:50 +0800

    Added playToFive function

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -54,6 +54,8 @@ function getWinner(playerMove,computerMove) {
         winner = "player";
     } else if ((playerMove = "scissors") && (computerMove = "paper")) {
         winner = "player";
+    } else {
+        winner = 'tie';
     }
     return winner;
 }
@@ -62,8 +64,19 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    var playerMove = getPlayerMove();
+    var computerMove = getComputerMove();
+    while (playerWins < 5 && computerWins <5) {
+        if (getWinner('player')) {
+            playerWins += 1;
+        } else if (getWinner('computer')) {
+            computerWins += 1;
+        } else {
+            playerWins +=0;
+            computerWins +=0;
+        }
+        console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+        console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+    }
     return [playerWins, computerWins];
 }
-
